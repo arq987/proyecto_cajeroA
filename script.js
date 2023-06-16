8f93637352741b4b6e9fabf27953d576d2da2d28
Ricardo Bello P <ricardobellop@gmail.com>
2023-06-15 22:06:58 -0500

    tercer commit cajeroA script

---
+++ script.js
@@ -0,0 +1,33 @@
+// Arreglo de cuentas, condicion inicial las deje var porque en el requerimiento decia var
+var cuentas = [
+    { nombre: "Mali", saldo: 200 },
+    { nombre: "Gera", saldo: 290 },
+    { nombre: "Maui", saldo: 67 }
+];
+
+// Variables globales
+var selectedAccount = null;
+var password = null;
+
+// Función para mostrar opciones después de ingresar el password
+function showOptions() {
+   
+   
+   
+}
+
+// Función para validar el password y mostrar opciones si es correcto
+function login() {
+    // Validar el password (puedes implementar tu propia lógica de validación aquí)
+}
+
+// Función para realizar un depósito
+function deposit() {
+    // Validar el monto (puedes agregar tus propias validaciones aquí)   
+    
+}
+
+// Función para realizar un retiro
+function withdraw() {
+    // Validar el monto (puedes agregar tus propias validaciones aquí)
+}
